a06de3ec85349cd006422c424ff5ae19fd631d4a
abduh3131 <94156984+abduh3131@users.noreply.github.com>
2023-10-10 14:31:54 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -2,37 +2,37 @@
 public class Product {
 
 
-    private String name;
-    private double price;
-    private int UPC;
+	private String name;
+	private double price;
+	private int UPC;
 
-    Product (int code, String name, double price) {
-        this.UPC=code;
-        this.name = name;
-        this.price = price;
-    }
+	Product (int code, String name, double price) {
+		this.UPC=code;
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public double getPrice() {
+		return price;
+	}
 
-    public double getPrice() {
-        return price;
-    }
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+	public int getUPC() {
+		return UPC;
+	}
 
-    public int getUPC() {
-        return UPC;
-    }
-
-    public void setUPC(int uPC) {
-        UPC = uPC;
-    }
+	public void setUPC(int uPC) {
+		UPC = uPC;
+	}
 }
