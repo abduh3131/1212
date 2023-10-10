3ef5b30a1932e0940abf4c686578025abaa783ae
abduh3131 <94156984+abduh3131@users.noreply.github.com>
2023-10-10 14:34:37 -0400

    Update Keyboard.java

--- Keyboard.java
+++ Keyboard.java
@@ -6,60 +6,67 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+// created a swing interface to read the UPC code and send it to the cash register
+
+
 public class Keyboard {
 
-	private JFrame frame;
-	private JLabel UPCLabel;
-	private JTextField UPCTextfield;
-	private JButton UPCButton;
-	private CashRegisterModel cashRegister;
+    private JFrame frame;
+    private JLabel UPCLabel;
+    private JTextField UPCTextfield;
+    private JButton UPCButton;
+    private CashRegisterModel cashRegister;
 
-	Keyboard(String title, CashRegisterModel register) {
-		this.cashRegister = register;
+    Keyboard(String title, CashRegisterModel register) {
 
-		frame = new JFrame(title);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 80);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+        this.cashRegister = register;
 
-		// Create UI elements
-		UPCLabel = new JLabel("UPC:");
-		UPCTextfield = new JTextField(15);  // set length for the text field
-		UPCButton = new JButton("Enter");
+        frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 80);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
-		// Add UI element to frame
-		GroupLayout layout = new GroupLayout(frame.getContentPane());
-		layout.setAutoCreateGaps(true);
-		layout.setAutoCreateContainerGaps(true);
+        // Create UI elements
+        UPCLabel = new JLabel("UPC:");
+        UPCTextfield = new JTextField();
+        UPCButton = new JButton("Enter");
 
-		layout.setHorizontalGroup(
-				layout.createSequentialGroup()
-						.addComponent(UPCLabel)
-						.addComponent(UPCTextfield)
-						.addComponent(UPCButton)
-		);
-		layout.setVerticalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-						.addComponent(UPCLabel)
-						.addComponent(UPCTextfield)
-						.addComponent(UPCButton)
-		);
+        // Add UI element to frame
 
-		frame.getContentPane().setLayout(layout);
+        GroupLayout layout = new GroupLayout(frame.getContentPane());
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
 
-		// set the keyboard to manage the UPC Enter button event
-		this.UPCButton.addActionListener(e -> processUPCCode());
-	}
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(UPCLabel))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(UPCTextfield))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(UPCButton)));
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(UPCLabel)
+                        .addComponent(UPCTextfield).addComponent(UPCButton)));
 
-	private void processUPCCode() {
-		try {
-			int upcCode = Integer.parseInt(UPCTextfield.getText());
-			cashRegister.processUPC(upcCode);
-		} catch (NumberFormatException e) {
-			System.err.println("Invalid UPC code entered.");
-		}
-	}
+
+        frame.getContentPane().setLayout(layout);
+
+        //	  set the keyboard to manage the UPC Enter button event
+
+        this.UPCButton.addActionListener(e -> processUPCCode());
+
+    }
+
+    // Keyboard.java
+
+    private void processUPCCode() {
+        try {
+            int upcCode = Integer.parseInt(UPCTextfield.getText());
+            cashRegister.processUPC(upcCode);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid UPC format. Please enter a numeric UPC.");
+        }
+    }
+
+
 
 
 
